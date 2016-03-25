72bd16dd435d12774c0d74553c3d97d01c4cfaa1
Zè Vinícius <jvmirca@gmail.com>
2016-03-24 21:37:08 -0300

    Update rejection_sampling.py

--- fsopy/simulation/rejection_sampling.py
+++ fsopy/simulation/rejection_sampling.py
@@ -26,22 +26,20 @@ def rejection_sampling(density, a, b, K, *args):
         1-D array containing ``K`` samples from the distribution ``density``. 
     """
     x = np.linspace(a, b, K)	
-	pdf = density(x, *args)
-	pdfmax = np.max(pdf)
+    pdf = density(x, *args)
+    pdfmax = np.max(pdf)
 
-	if np.isnan(pdfmax) or np.isinf(pdfmax):
-		return "error(rejection_sampling): pdfmax is NaN."
-
-	K_min = 0
-	rvs = np.zeros(1)
-	while K_min < K+1:
-		u1 = stats.uniform.rvs(loc=a,scale=b-a,size=K)
-		u2 = stats.uniform.rvs(size=K)
-
-		idx = np.where(u2<=density(u1,*args)/pdfmax)[0]
-		rvs = np.concatenate([rvs,u1[idx]])
-
-		K_min = np.size(rvs)
+    if np.isnan(pdfmax) or np.isinf(pdfmax):
+    	return "error(rejection_sampling): pdfmax is NaN."
+    
+    K_min = 0
+    rvs = np.zeros(1)
+    while K_min < K+1:
+    	    u1 = stats.uniform.rvs(loc=a,scale=b-a,size=K)
+    	    u2 = stats.uniform.rvs(size=K)
+    	    idx = np.where(u2<=density(u1,*args)/pdfmax)[0]
+    	    rvs = np.concatenate([rvs,u1[idx]])
+    	    K_min = np.size(rvs)
 	
-	rvs = np.reshape(rvs,[K_min,1])
-	return rvs[1:K+1]
+    rvs = np.reshape(rvs,[K_min,1])
+    return rvs[1:K+1]
